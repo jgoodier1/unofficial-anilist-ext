33ba55f2b6007f91de4ef9d691daf934c2507736
Jacob Goodier <jgoodier21@gmail.com>
2021-04-21 11:02:32 -0400

    got the markdown parser to work

--- popup/webComponents.js
+++ popup/webComponents.js
@@ -724,7 +724,24 @@ export class ParsedMarkdown extends HTMLElement {
     paragraph.setAttribute('class', 'paragraph');
 
     const style = document.createElement('style');
-    style.textContent = `.paragraph{margin-top: 0;}`;
+    style.textContent = `
+      .paragraph{
+        margin-top: 0;
+      }
+      .spoiler::before {
+        content: "SPOILER, HOVER TO VIEW";
+        cursor: pointer;
+        padding: 8px;
+        font-size: 16px;
+        font-weight: 600;
+      }
+      .hidden {
+        display: none;
+      }
+      .spoiler:hover > .hidden {
+        display: inline;
+      }
+      `;
 
     this.shadowRoot.append(paragraph, style);
   }
@@ -734,7 +751,17 @@ export class ParsedMarkdown extends HTMLElement {
 
     const data = this.getAttribute('data');
 
-    const newData = data.replaceAll(/\n/g, '<br>');
+    const newData = data
+      .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>')
+      .replace(/__(.*)__/gim, '<strong>$1</strong>')
+      .replace(/\*(.*)\*/gim, '<em>$1</em>')
+      .replace(/_(.*)_/gim, '<em>$1</em>')
+      .replace(
+        /~!(.*)!~/gim,
+        `<span class="spoiler"><br><span class='hidden'>$1</span><br></span>`
+      )
+      .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2">$1</a>')
+      .replace(/\n/g, '<br>');
     paragraph.innerHTML = newData;
   }
 }
