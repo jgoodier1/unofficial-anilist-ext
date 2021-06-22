8e64ff220434d5f52cb530f17f37e5158e03269c
Jacob Goodier <jgoodier21@gmail.com>
2021-06-22 09:42:02 -0400

    finished home card

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -35,8 +35,12 @@ interface Entry {
   };
 }
 
-interface StylingProps {
+interface MediaInfoStylingProps {
   position: 'left' | 'right';
+}
+
+interface EpisodeStylingProps {
+  behind: boolean;
 }
 
 const UPDATE_ENTRY = gql`
@@ -65,19 +69,62 @@ const HomeCard: React.FC<HomeCardProps> = ({ entry, index }) => {
   const totalContent =
     entry.media.type === 'ANIME' ? entry.media.episodes : entry.media.chapters;
 
+  let timeToNextEpisode;
+  if (entry.media.nextAiringEpisode && entry.media.nextAiringEpisode.timeUntilAiring) {
+    const DAY = 86400;
+    const HOUR = 3600;
+    const MINUTE = 60;
+    const days = Math.trunc(entry.media.nextAiringEpisode.timeUntilAiring / DAY);
+    const dayRemainder = entry.media.nextAiringEpisode.timeUntilAiring % DAY;
+    const hours = Math.trunc(dayRemainder / HOUR);
+    const hourRemainder = dayRemainder % HOUR;
+    const minutes = Math.trunc(hourRemainder / MINUTE);
+
+    if (days === 0) {
+      if (hours === 0) timeToNextEpisode = `${minutes}m`;
+      else if (minutes === 0) timeToNextEpisode = `${hours}h`;
+      else timeToNextEpisode = `${hours}h ${minutes}m`;
+    } else {
+      if (hours === 0 && minutes === 0) {
+        timeToNextEpisode = `${days}d`;
+      } else if (minutes === 0) timeToNextEpisode = `${days} ${hours}h`;
+      else if (hours === 0) timeToNextEpisode = `${days}d ${minutes}m`;
+      else timeToNextEpisode = `${days}d ${hours}h ${minutes}m`;
+    }
+  }
+
+  let episodesBehind;
+  let episodesBehindBool = false;
+  if (
+    entry.media.nextAiringEpisode &&
+    entry.media.nextAiringEpisode.episode &&
+    entry.media.nextAiringEpisode.episode - entry.progress > 1
+  ) {
+    episodesBehind = entry.media.nextAiringEpisode.episode - entry.progress - 1;
+    episodesBehindBool = true;
+  }
+
   const position = LEFT_POSITIONS.includes(index + 1) ? 'left' : 'right';
 
   return (
     <CardWrapper>
       <StyledLink
-        to={`/media?id=${entry.id}`}
+        to={`/media/${entry.id}`}
         style={{ backgroundImage: `url(${entry.media.coverImage.medium})` }}
       ></StyledLink>
       <Updater onClick={updateHandler}>
         {loading ? 'Loading...' : entry.progress + ' +'}
       </Updater>
-      {/* TODO: add time till next episode */}
+      {entry.media.nextAiringEpisode && entry.media.nextAiringEpisode.episode && (
+        <EpisodeWrapper behind={episodesBehindBool}>
+          <EpisodeParagraph>Ep: {entry.media.nextAiringEpisode.episode}</EpisodeParagraph>
+          <EpisodeParagraph>{timeToNextEpisode}</EpisodeParagraph>
+        </EpisodeWrapper>
+      )}
       <MediaInformation position={position}>
+        {episodesBehindBool && (
+          <EpisodesBehind>{episodesBehind} episode behind</EpisodesBehind>
+        )}
         <Title>{entry.media.title.userPreferred}</Title>
         <Progress>
           Progress: {entry.progress} {totalContent && '/ ' + totalContent}
@@ -135,7 +182,39 @@ const Updater = styled.div`
   }
 `;
 
-const MediaInformation = styled.div<StylingProps>`
+const EpisodeWrapper = styled.div<EpisodeStylingProps>`
+  opacity: 1;
+  border-bottom: 4px #2f2e2ec4;
+  height: 45px;
+
+  isolation: isolate;
+  position: absolute;
+  text-align: center;
+  background: #2e2d2dc4;
+  width: 85px;
+  color: white;
+  font-size: 12px;
+  transition: 0.2s;
+  border-radius: 0 0 3px 3px;
+  bottom: 0;
+  font-weight: 300;
+  border-bottom: ${props => (props.behind ? '4px solid #ff6d6d' : 'none')};
+
+  ${StyledLink}:hover ~ &,
+  ${StyledLink}:focus ~ &,
+  ${Updater}:hover ~ &,
+  ${Updater}:focus ~ &,
+  &:hover {
+    opacity: 0;
+    height: 0px;
+  }
+`;
+
+const EpisodeParagraph = styled.p`
+  margin: 5px 0;
+`;
+
+const MediaInformation = styled.div<MediaInfoStylingProps>`
   z-index: 5;
   display: none;
   background: #edf1f5;
@@ -158,6 +237,12 @@ const MediaInformation = styled.div<StylingProps>`
   /* ${Updater}:focus ~ &  */ {
     display: grid;
   }
+`;
+
+const EpisodesBehind = styled.p`
+  color: #005fc4;
+  font-size: 12px;
+  margin: 0;
 `;
 
 const Title = styled.p`
